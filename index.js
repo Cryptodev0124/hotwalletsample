2fc1e52795bdd96b9cc661a2193be7724dcb1931
Cryptodev0124 <cryptodev0124@gmail.com>
2024-11-06 06:19:39 +1000

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-import { hotProvider } from "./adapter/evm";
-import { HotWalletAdapter as SolanaHotWalletAdapter } from "./adapter/solana";
-import { setupHotWallet } from "./adapter/near";
-import "./adapter/ton";
-export { AuthPayload, authPayloadSchema, verifySignature } from "./helpers/nep0314";
-export { SolanaHotWalletAdapter, hotProvider };
-export { setupHotWallet };
-export { default as HOT, RequestFailed } from "./hot";
-//# sourceMappingURL=index.js.map
